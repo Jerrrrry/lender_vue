141bbce46c36679b38692d2b7a6ab346db89efb3
Yubin QIU <Yubin.Qiu@kp.org>
2018-12-23 11:55:29 -0800

    init readme

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 let isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  REST_ENDPOINT: JSON.stringify('http://loveplanet.live/wp-json/wp/v2'),
+  REST_ENDPOINT: JSON.stringify('http://104.248.73.159/api'),
   POSTS_PER_PAGE: JSON.stringify(9),
   GA_TRACKING_ID: JSON.stringify('UA-129495864-1'),
   REQUEST_CACHE_MAX: JSON.stringify(150),
-  SITE_NAME:JSON.stringify("Love Planet"),
-  //https://wptavern.com/wp-json/wp/v2
-  //http://loveplanet.live/wp-json/wp/v2
+  SITE_NAME:JSON.stringify("Lender UI"),
+  
 };
